c02397bdcdd51ccca28875ace2fccd9c140220c7
Jack Caltagirone <35774493+JackCaltagirone@users.noreply.github.com>
2019-03-30 12:09:05 -0700

    fuctioning thompson

--- project.py
+++ project.py
@@ -8,9 +8,9 @@
 
 
 # these two lines just ask the user for and expression to make
-# into a NFA, and get a string to compare against 
+# into a NFA, and get a string to compare against
 userExpression = input("Please enter a infix Expression: ")
-#userString = input("Please enter a string to compare against the NFA: ")
+userString = input("Please enter a string to compare against the NFA: ")
 
 
 def shunt(infix):
@@ -18,27 +18,27 @@ def shunt(infix):
     specials = {'*': 3, '.': 2, '|': 1}
     # creates dictionary
 
-    #making the profix and stack variales
+    # making the profix and stack variales
     postfix = ""
     stack = ""
 
-    #forloop that will parse the expression
+    # forloop that will parse the expression
     for c in infix:
-        #first if takes the opening bracket for the equation and adds it to the stack
+        # first if takes the opening bracket for the equation and adds it to the stack
         if c == '(':
             stack = stack + c
-        #since the open bracket is on the stack, if the for loop encounters a close bracket we check the stack
-        #going to then check the stack all the way down until we find the open braket in the stack.
+        # since the open bracket is on the stack, if the for loop encounters a close bracket we check the stack
+        # going to then check the stack all the way down until we find the open braket in the stack.
         elif c == ')':
-            #while loops through the stack until it meets a the open bracket. it also adds everything that it meets along
-            #the way to to the postfix variable.
+            # while loops through the stack until it meets a the open bracket. it also adds everything that it meets along
+            # the way to to the postfix variable.
             while stack[-1] != '(':
                 postfix = postfix + stack[-1]
                 stack = stack[:-1]
-            #line below gets rid of the open bracket that we just read in.
+            # line below gets rid of the open bracket that we just read in.
             stack = stack[:-1]
         elif c in specials:
-            #if theres anything on the stack, this while will run.
+            # if theres anything on the stack, this while will run.
             while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
                 postfix = postfix + stack[-1]
                 stack = stack[:-1]
@@ -46,16 +46,92 @@ def shunt(infix):
         else:
             postfix = postfix + c
 
-    #if there is anything on the stack after running through the for loop, you need to add them to the 
-    #end of the postfix.
+    # if there is anything on the stack after running through the for loop, you need to add them to the
+    # end of the postfix.
     while stack:
         postfix = postfix + stack[-1]
         stack = stack[:-1]
-    
+
+    compile(postfix)
+
     return postfix
 
-print(shunt(userExpression))
+
+#############################################################################################################################
+# above is the shunting. works by taking in the infix expression and parsing it through to postfix
+
+class state:
+    label = None
+    edge1 = None
+    edge2 = None
+
+# this class just makes the variables for the nfa. the accept state and the initial state
 
 
+class nfa:
+    initial = None
+    accept = None
+
+    def __init__(self, initial, accept):  # first argument is 'self'
+        self.initial = initial
+        self.accept = accept
 
 
+def compile(postfix):
+    nfstack = []
+
+    for c in postfix:
+
+        if c == '.':
+            # pop two nfas off the stack
+            nfa2 = nfstack.pop()
+            nfa1 = nfstack.pop()
+            # connect first nfas accept state to the seconds intial
+            nfa1.accept.edge1 = nfa2.initial
+            # push nfa to the stack
+            newNfa = nfa(nfa1.initial, nfa2.accept)
+            nfstack.append(newNfa)
+
+        elif c == '|':
+            # pop two nfas off the stack
+            nfa2 = nfstack.pop()
+            nfa1 = nfstack.pop()
+            # create a new initial state, connect it to initial states
+            # of the two nfas popped from the stack
+            initial = state()
+            initial.edge1 = nfa1.initial
+            initial.edge2 = nfa2.initial
+            # create a new accept state, connecting the accept states
+            # of the two nfa's popped of the stack, to the new state
+            accept = state()
+            nfa1.accept.edge1 = accept
+            nfa2.accept.edge2 = accept
+            # push new nfa to the stack
+            newNfa = nfa(nfa1.initial, nfa2.accept)
+            nfstack.append(newNfa)
+
+        elif c == '*':
+            # pop a single nfa from the stack
+            nfa1 = nfstack.pop()
+            # create new initial and accpet states
+            initial = state()
+            accept = state()
+            # join the new initial state to nfas initial state and the new acept state
+            initial.edge1 = nfa1.initial
+            initial.edge2 = accept
+            # join old accept state to the new accept state and nfa1's initial state
+            nfa1.accept.edge1 = nfa1.initial
+            nfa1.accept.edge2 = accept
+            # push the new nfa to the stack
+            newNfa = nfa(nfa1.initial, nfa2.accept)
+            nfstack.append(newNfa)
+
+        else:
+            accept = state()
+            initial = state()
+            initial.label = c
+            initial.edge1 = accept
+            nfstack.append(nfa(initial, accept))
+
+    # should only have a single nfa
+    return nfstack.pop()
