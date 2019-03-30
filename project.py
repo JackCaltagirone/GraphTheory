4a8a8e0a81949ce6939d67b3f51aa36ad564ce01
Jack Caltagirone <35774493+JackCaltagirone@users.noreply.github.com>
2019-03-30 08:01:29 -0700

    added user input.
    I didnt want to just paste code so I started off with just adding in simple user input

--- project.py
+++ project.py
@@ -1,48 +1,12 @@
 #Name: Jack Caltagirone
 #Number: G00349976
 
-#this is my graph theory project. Please check read me about running the
-#program along for notes on it. 
+#create a python program that lets the user input an expression, and a string. the program will create a NFA from the 
+#expression, and compare it against the string
 
-#I'll have most of the code comments here for single lines and methods but 
-#most of the explantion will be done in the read me.
+#i'll comment all unobvious lines of code. for more information please see readme
 
-#basic input and puts the expression from infix to profix.
-def shunt(infix):
 
-    specials = {'*': 50, '.': 40, '|': 30}
-    # creates dictionary
-    # prioritize *, then . , then | 
+userExpression = input("Please enter a infix Expression: ")#these two lines just ask the user for and expression to make
+userString = input("Please enter a string to compare against the NFA: ")#into a NFA, and get a string to compare against
 
-    postfix = ""
-    stack = ""
-
-    #for statment
-    for c in infix:
-
-        #if statment with elifs and else at the bottom.
-        if c == '(':
-            stack = stack + c
-            #adds the open braket to the stack
-        elif c == ')':
-            #if the input is a close braket then part of the expression is complete
-            #it has to take the two brakets off and just add the alphbet t
-            while stack[-1] != '(':
-                postfix = postfix + stack[-1]
-                stack = stack[:-1]
-            stack = stack[:-1]
-        elif c in specials:
-            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
-                postfix = postfix + stack[-1]
-                stack = stack[:-1]
-            stack = stack + c
-        else:
-            postfix = postfix + c
-
-    while stack:
-        postfix = postfix + stack[-1]
-        stack = stack[:-1]
-
-    return postfix
-
-print(shunt("(a.b)|(a*.b*)"))
